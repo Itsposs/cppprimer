3f2149ea69d5f650e98d25a78cd13ca5f911d989
hvate huo <Itsposs@163.com>
2019-09-21 07:42:14 +0800

    add function.cc

---
+++ ch06/function.cc
@@ -0,0 +1,12 @@
+
+bool lengthCompare(const std::string &s1, const std::string &s2)
+{
+	return s1.size() > s2.size();
+}
+
+
+void useBigger(const std::string &s1, const std::string &s2
+	bool (*pf)(const std::string &, const std::string &))
+{
+	
+}
