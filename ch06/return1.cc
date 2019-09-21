ee4edc089c81f0723b4725219ba8b3bd5c8d4553
hvate <hvate@localhost.localdomain>
2019-09-21 07:01:54 -0400

    add quick.cc

---
+++ ch06/return1.cc
@@ -0,0 +1,20 @@
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+std::vector<std::string> func(std::string &s)
+{
+	if ( s = std::string("hello"))
+		return {"ok,", " hello"};
+	else
+		return {"error,", "hello"};
+}
+
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
