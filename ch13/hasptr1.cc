d547e238a968f2a33cc112cb6eb7eeb5a2219dd3
hvate huo <itsposs@163.com>
2019-10-01 23:45:43 +0800

    delete .swp

--- ch13/hasptr1.cc
+++ ch13/hasptr1.cc
@@ -4,16 +4,14 @@
 
 
 
-class HasPtr
-{
+class HasPtr {
+	friend void print(std::ostream &os, HasPtr &s);
 	public:
-		friend void print(std::ostream &os, HasPtr &s);
-		HasPtr(const std::string &s = std::string()):
-
-			ps(new std::string(s)), i(0) { }
+		HasPtr(const std::string &s = std::string())
+			: ps(new std::string(s)), i(0) { }
 		// 对ps指向的string,每个HasPtr对象都有自己的拷贝
-		HasPtr(const HasPtr &p):
-			ps(new std::string(*p.ps)), i(p.i) { }
+		HasPtr(const HasPtr &p)
+			: ps(new std::string(*p.ps)), i(p.i) { }
 		HasPtr& operator=(const HasPtr &);
 		~HasPtr() { delete ps; }
 		std::string& operator*() { return *ps; }  // 解引用
@@ -23,14 +21,12 @@ class HasPtr
 		int i;
 };
 
-void print(std::ostream &os, HasPtr &s)
-{
+void print(std::ostream &os, HasPtr &s) {
 	os << *s.ps << " " << s.i << std::endl;
 }
 
 
-HasPtr& HasPtr::operator=(const HasPtr &rhs)
-{
+HasPtr& HasPtr::operator=(const HasPtr &rhs) {
 	auto newp = new std::string(*rhs.ps);  // 拷贝底层string
 	delete ps;  // 释放旧内存
 	ps = newp;
@@ -38,19 +34,18 @@ HasPtr& HasPtr::operator=(const HasPtr &rhs)
 	return *this;  // 返回本对象
 }
 
-void testHasPtr()
-{
+void testHasPtr() {
 	std::string s{"hello world!"};
 	HasPtr ptr1(s);
 	HasPtr ptr2 = ptr1;
 
 	print(std::cout, ptr1);
 	print(std::cout, ptr2);
-}
+
+	}
 
 
-int main(int argc,char *argv[])
-{
+int main(int argc,char *argv[]) {
 	using namespace std::chrono;
 	auto begin = high_resolution_clock::now();
 	
