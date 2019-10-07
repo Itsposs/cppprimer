8bdf14a68d8c759594c59632b9430fa5e75d4de6
hvate.huo <31950815+Itsposs@users.noreply.github.com>
2019-10-07 12:55:45 +0800

    create accessible3.cc

---
+++ ch15/accessible3.cc
@@ -0,0 +1,33 @@
+class Base {
+public:
+    void pub_mem();
+
+protected:
+    int prot_mem;
+private:
+    char priv_mem;
+};
+
+struct Pub_Derv : public Base {
+    int f() { return prot_mem; }
+    char g() { return priv_mem; }  // error
+};
+
+struct Priv_Derv : private Base {
+    int f1() const { return prot_mem; }
+};
+
+struct Derived_from_Public : public Pub_Derv {
+    int use_base() { return prot_mem; }
+};
+
+struct Derived_from_Private : public Priv_Derv {
+    int use_base() { return prot_mem; }  // error
+};
+
+void test() {
+    Pub_Derv d1;
+    Priv_Derv d2;
+    d1.pub_mem();
+    d2.pub_mem();  // error
+}
